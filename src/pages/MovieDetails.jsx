403e71d0755e7e3bc6decb81fd3c095950a81edf
Gabriel Chaves <gabovvv@gmail.com>
2022-10-09 20:46:40 -0600

    update movie details

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -3,42 +3,59 @@ import { useParams } from "react-router-dom";
 import { Spinner } from "../components/Spinner";
 import { getMovieImg } from "../utils/getMovieImg";
 import { getHttp } from "../utils/httpClient";
-import styles from "./MovieDetails.module.css"
+import styles from "./MovieDetails.module.css";
 
+export function MovieDetails() {
+  const { movieId } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
 
-export function MovieDetails(){
-    const {movieId} = useParams();
-    const [isLoading, setIsLoading] = useState(true);
-    const [movie, setMovie] =  useState(null);
+  useEffect(() => {
+    setIsLoading(true);
+    getHttp("/movie/" + movieId).then((data) => {
+      setMovie(data);
+      setIsLoading(false);
+    });
+  }, [movieId]);
 
- 
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-    useEffect(() => {
-        setIsLoading(true);
-        getHttp("/movie/" + movieId).then((data) => {
-            setMovie(data);
-            setIsLoading(false);
-        }); 
-    }, [movieId]);
+  if (!movie) {
+    return null;
+  }
 
-    if(isLoading){
-        return <Spinner/>;
-    }
-
-    if(!movie){
-        return null;
-    }
-
-    const imageURL = getMovieImg(movie.poster_path,500)
-    return <div className={styles.detailsContainer}>
-        <img className={`${styles.col} ${styles.movieImage}`} src={imageURL} alt={movie.title}/>
-        <div className={`${styles.col} ${styles.movieDetails}`}>
-            <p className={styles.firstItem}><strong>Title:</strong> {movie.title}</p>
-            <p>
-            <strong>Genres: </strong>
-                {movie.genres.map(genre => genre.name).join(", ")}
-            </p>
-            <p><strong>Description:</strong> {movie.overview}</p>
-        </div>
-        </div>
+  const imageURL = getMovieImg(movie.poster_path, 500);
+  return (
+    <div className={styles.detailsContainer}>
+      <img
+        className={`${styles.col} ${styles.movieImage}`}
+        src={imageURL}
+        alt={movie.title}
+      />
+      <div className={`${styles.col} ${styles.movieDetails}`}>
+        <p className={styles.firstItem}>
+          <strong>Title:</strong> {movie.title}
+        </p>
+        <p>
+          <strong>Genres: </strong>
+          {movie.genres.map((genre) => genre.name).join(", ")}
+        </p>
+        <p>
+          <strong>Description:</strong> {movie.overview}
+        </p>
+        <p>
+          <strong>Votes:</strong> {movie.vote_average}
+        </p>
+        <p>
+          <strong>Realese Date:</strong> {movie.release_date}
+        </p>
+        <p>
+          <strong>Production: </strong>
+          {movie.production_companies.map((production) => production.name).join(", ")}
+        </p>
+      </div>
+    </div>
+  );
 }
